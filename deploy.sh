571b0d9948248357f2c448faa7bdb058ed61d97e
Guillaume Delacour <gui@iroqwa.org>
2020-06-26 01:19:40 +0200

    Tweaks

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ echo "[+] Build datas & index.html"
 echo "[+] Publish index.html to S3"
 AWS_PROFILE=aws-managed-services-versions aws --region eu-west-3 s3 cp index.html s3://aws-managed-services-versions/
 echo "[+] CloudFront cache purge"
-sed --in-place "s/<id>/$(date +%s)/" invalidation-batch.json
+sed "s/<id>/$(date +%s)/" invalidation-batch_template.json > invalidation-batch.json
 aws cloudfront create-invalidation --distribution-id E3AS59ZAE6FV3Z --invalidation-batch file://invalidation-batch.json
