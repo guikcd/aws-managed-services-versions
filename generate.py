e24080e375a17cdc2f68a62553828d208317dd5b
Guillaume Delacour <gui@iroqwa.org>
2020-09-22 21:25:53 +0200

    Add docstring

--- generate.py
+++ generate.py
@@ -186,6 +186,9 @@ def lambda_versions():
     return test_versions(versions)
 
 def opsworks_puppet_versions():
+    """
+    OpsWorks Puppet
+    """
     req = requests.get(VERSION_URL_DETAIL["opsworks-puppet"])
     soup = BeautifulSoup(req.text, 'html.parser')
     result = soup.find_all("div", attrs={"id": "main-col-body"})
@@ -195,6 +198,9 @@ def opsworks_puppet_versions():
     return test_versions(versions)
 
 def opsworks_chef_versions():
+    """
+    OpsWorks Puppet
+    """
     req = requests.get(VERSION_URL_DETAIL["opsworks-chef"])
     soup = BeautifulSoup(req.text, 'html.parser')
     components = soup.find_all("table", attrs={"id": "w336ab1b9b9"})
