6522064fc1755bcdaab8a1c49237cc527602ddff
Guillaume Delacour <delacoug@amazon.com>
2020-11-05 14:28:06 +0100

    Add Amazon MQ RabbitMQ engine

--- test_lambda.sh
+++ test_lambda.sh
@@ -1,1 +1,1 @@
-OUTPUT_BUCKET=aws-managed-services-versions OUTPUT_FILE=index2.html python3 generate.py
+LOCAL_DEBUG=true OUTPUT_BUCKET=aws-managed-services-versions OUTPUT_FILE=index2.html python3 generate.py
