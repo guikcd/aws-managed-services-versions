93c740178a45cc7d98e390bb8d0571cd0e5f9dc6
Guillaume Delacour <guillaume.delacour@gmail.com>
2022-11-03 23:35:28 +0100

    Break test to validate behavior

--- e2e/tests.spec.ts
+++ e2e/tests.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
 
-  await page.goto('https://aws-versions.iroqwa.org/');
+  await page.goto('https://aws-versions.iroqwa.org/index2.html');
 
   await expect(page).toHaveTitle(/Amazon Managed Services versions/);
 
   // TODO: remove?
   await page.getByLabel('Search:').click();
 
-  await page.getByLabel('Search:').fill('rabbitmq');
+  await page.getByLabel('Search:').fill('raabbitmq');
 
   await page.getByRole('cell', { name: 'Amazon MQ for RabbitMQ' });
 
