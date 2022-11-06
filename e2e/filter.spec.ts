3145556ec8c5d77e87b6a321f29f1d4cd9ad9ff5
Guillaume Delacour <guillaume.delacour@gmail.com>
2022-11-06 23:30:43 +0100

    Add end to end tests (#100)
    * Add end to end tests
    * Set dependabot automerge (see Setting in this repo)
    * Verify date of generated page

---
+++ e2e/filter.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+
+test('Check filter with kubernetes', async ({ page }) => {
+  await page.goto('https://aws-versions.iroqwa.org/index2.html')
+
+  await page.getByLabel('Search:').fill('kubernetes')
+
+  await expect(page.getByText('Amazon Elastic Kubernetes Service (Amazon EKS)').first(), 'should match EKS').toContainText('EKS')
+})
